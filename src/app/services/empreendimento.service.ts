2f1cea26a08b899c0a03b18a6f507943838ed8b3
Mario Cesar <mc.fialho1@gmail.com>
2025-01-16 20:57:03 -0300

    refactor: remove cache do getAll

--- src/app/services/empreendimento.service.ts
+++ src/app/services/empreendimento.service.ts
@@ -1,10 +1,10 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { EmpreendimentoDto } from '../dto/empreendimento.dto';
+import { SearchDTO } from '../dto/searchDTO';
 import { API_PATH } from '../environment/environment';
 import { Empreendimento } from '../intefaces/empreendimento.interface';
-import { SearchDTO } from '../dto/searchDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +18,10 @@ export class EmpreendimentoService {
 
   getAll(searchDTO: SearchDTO): Observable<{ data: Empreendimento[] }> {
     let params = new HttpParams();
+    const headers = new HttpHeaders({
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache'
+  });
 
     if (searchDTO.nome_fantasia) {
       params = params.set('nome_fantasia', searchDTO.nome_fantasia);
@@ -32,7 +36,7 @@ export class EmpreendimentoService {
       params = params.set('situacao', searchDTO.situacao.toString());
     }
 
-    return this.httpClient.get<{ data: Empreendimento[] }>(`${API_PATH}/empreendimento`, { params });
+    return this.httpClient.get<{ data: Empreendimento[] }>(`${API_PATH}/empreendimento`, { headers, params });
   }
 
   getById(id: string) {
